b6ec3f45f9d0effecd450e3d9fc1af3232b917b3
Dmitrii Okhlopkov <91318266+tmokkuss@users.noreply.github.com>
2023-04-06 18:09:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,11 @@ import numpy as np
 chat_id = 861665812
 
 def solution(x: np.array) -> float:
-    t = 47
-    return (2 / t**2) * np.mean(x)
+    n = x.size
+    t = 68
+    sum_x = x.sum()
+    x_mean = sum_x / (n * t)
+    return x_mean
 
 
 
