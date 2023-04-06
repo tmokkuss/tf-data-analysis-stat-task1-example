7fa4f29b76f76953f973fab7ac0c559d02e1f694
Dmitrii Okhlopkov <91318266+tmokkuss@users.noreply.github.com>
2023-04-06 14:56:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,11 +5,8 @@ import numpy as np
 chat_id = 861665812
 
 def solution(x: np.array) -> float:
-    # Оцениваем параметры распределения
     s, loc, scale = lognorm.fit(x, floc=0)
-    # Вычисляем оценку параметра a
     x = np.log(s**2 / np.sqrt(scale**2 + loc**2))
-    # Возвращаем оценку параметра a
     return x.mean()
 
 
