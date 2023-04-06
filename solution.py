5991defe842a8804ef7d31dcf02e3729db6a2b83
Dmitrii Okhlopkov <91318266+tmokkuss@users.noreply.github.com>
2023-04-06 14:55:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 861665812
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    # Оцениваем параметры распределения
+    s, loc, scale = lognorm.fit(x, floc=0)
+    # Вычисляем оценку параметра a
+    x = np.log(s**2 / np.sqrt(scale**2 + loc**2))
+    # Возвращаем оценку параметра a
+    return x.mean()
+
+
+
